f0c60ce7ab635e434e4f31c0e18c20915ca06ec9
leandrotalenton <leandrotalenton@hotmail.com>
2022-02-16 20:02:49 -0300

    se ordenan las variables dentro de objetos para facilitar la lectura

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,30 +1,37 @@
-// recoleccion estimada por segundo de recursos (DATO)
-const foodCollectionRate = 0.3383;
-const woodCollectionRate = 0.41;
-const goldCollectionRate = 0.38;
-const stoneCollectionRate = 0.36;
-
-// variables para almacenar requisitos de aldeanos
-let totalFoodPerSecond = 0
-let totalWoodPerSecond = 0
-let totalGoldPerSecond = 0
-let totalStonePerSecond = 0
-
-// declaro variables que voy a utilizar en la funcion
-let foodVills
-let woodVills
-let goldVills
-let stoneVills
-
+// objeto recoleccion estimada por segundo de recursos (DATO)
+let collectionRates = {
+    food: 0.3383,
+    wood: 0.41,
+    gold: 0.38,
+    stone: 0.36,
+    // researchWheelBarrowUpgrade: function(){
+    //     collectionRates.food = 0.38;
+    // },
+    // researchHandCartUpgrade: function(){
+    //     collectionRates.food = 0.4;
+    // },
+}
+// objeto para almacenar requisitos de recursos por segundo
+let resourcesRquiredPerSecond = {
+    food: 0,
+    wood: 0,
+    gold: 0,
+    stone: 0,
+}
 // declaro la cartera de produccion donde se van a agregar las opcciones elegidas de unidades
 let carteraDeProduccion = []
-
-//calculo de aldeanos necesarios para mantener la produccion constante de unidades elegidas
-function calularCantidadDeAldeanos(){
-    foodVills = Math.ceil(totalFoodPerSecond / foodCollectionRate)
-    woodVills = Math.ceil(totalWoodPerSecond  / woodCollectionRate)
-    goldVills = Math.ceil(totalGoldPerSecond  / goldCollectionRate)
-    stoneVills = Math.ceil(totalStonePerSecond  / stoneCollectionRate)
+// declaro variables que voy a utilizar en la funcion
+let villagerDistribution = {
+    food: 0,
+    wood: 0,
+    gold: 0,
+    stone: 0,
+    calularCantidadDeAldeanos: function() {
+        villagerDistribution.food = Math.ceil(resourcesRquiredPerSecond.food / collectionRates.food);
+        villagerDistribution.wood = Math.ceil(resourcesRquiredPerSecond.wood  / collectionRates.wood);
+        villagerDistribution.gold = Math.ceil(resourcesRquiredPerSecond.gold  / collectionRates.gold);
+        villagerDistribution.stone = Math.ceil(resourcesRquiredPerSecond.stone  / collectionRates.stone);
+    },
 }
 
 // constructor de unidades (DATO)
@@ -38,15 +45,15 @@ class Unidad {
         this.stoneCost = stoneCost;
     }
 }
-const archer= new Unidad (`archer`,35,0,25,45,0);
-const knight= new Unidad (`knight`,30,60,0,75,0);
-const scoutCavalry= new Unidad (`scoutCavalry`,30,80,0,0,0);
-const villager= new Unidad (`villager`,25,50,0,0,0);
+const archer = new Unidad (`archer`,35,0,25,45,0);
+const knight = new Unidad (`knight`,30,60,0,75,0);
+const scoutCavalry = new Unidad (`scoutCavalry`,30,80,0,0,0);
+const villager = new Unidad (`villager`,25,50,0,0,0);
 
 // input de unidades deseadas a calcular
 let input
 do{
-    input = parseInt(prompt(`Agregue las unidades a producir (una por vez): 1:Knight, 2:Archer, 3:Scout Cavalry, 4:Aldeano, 5: terminar`))
+    input = parseInt(prompt(`Agregue las unidades a producir (una por vez): 0: terminar, 1:Knight, 2:Archer, 3:Scout Cavalry, 4:Aldeano`))
     if (input === 1){
         carteraDeProduccion.push(knight)
     } else if (input === 2){
@@ -55,29 +62,26 @@ do{
         carteraDeProduccion.push(scoutCavalry)
     } else if (input === 4){
         carteraDeProduccion.push(villager)
-    } else {
-        continue
-    }
-} while (input != 5)
+    } 
+} while (input != 0)
 
 // se escribe una lista de unidades a producir y se suma su costo por recurso por segundo en las variables declaradas anteriormente
 document.write(`<b>Calculadora de recursos AoE2</b> <br><br>Las unidades a producir son: <br>`)
+
 for(unidades of carteraDeProduccion){
     document.write(`${unidades.name}, `)
-    totalFoodPerSecond += (unidades.foodCost / unidades.timeCost)
-    totalWoodPerSecond += (unidades.woodCost / unidades.timeCost)
-    totalGoldPerSecond += (unidades.goldCost / unidades.timeCost)
-    totalStonePerSecond += (unidades.stoneCost / unidades.timeCost)
+    resourcesRquiredPerSecond.food += (unidades.foodCost / unidades.timeCost)
+    resourcesRquiredPerSecond.wood += (unidades.woodCost / unidades.timeCost)
+    resourcesRquiredPerSecond.gold += (unidades.goldCost / unidades.timeCost)
+    resourcesRquiredPerSecond.stone += (unidades.stoneCost / unidades.timeCost)
 }
 
-// document.write(`<br><br>Recursos requeridos por segundo: <br>food: ${totalFoodPerSecond} <br>wood: ${totalWoodPerSecond} <br>gold: ${totalGoldPerSecond} <br>stone: ${totalStonePerSecond}`)
-
-// se corre la formula calcular y se escriben los resultados
-calularCantidadDeAldeanos()
+// se ejecuta el metodo calularCantidadDeAldeanos y se escriben los resultados
+villagerDistribution.calularCantidadDeAldeanos();
 
 document.write(`<br><br>la cantidad minima de aldeanos necesarios para mantener la produccion constante de estas unidades es:<br>
-Aldeanos en comida ${foodVills} <br>
-Aldeanos en madera ${woodVills} <br>
-Aldeanos en oro ${goldVills} <br>
-Aldeanos en piedra ${stoneVills}<br><br>
-Aldeanos totales requeridos: ${foodVills+woodVills+goldVills+stoneVills}`)
+Aldeanos en comida ${villagerDistribution.food} <br>
+Aldeanos en madera ${villagerDistribution.wood} <br>
+Aldeanos en oro ${villagerDistribution.gold} <br>
+Aldeanos en piedra ${villagerDistribution.stone}<br><br>
+Aldeanos totales requeridos: ${villagerDistribution.food+villagerDistribution.wood+villagerDistribution.gold+villagerDistribution.stone}`)
