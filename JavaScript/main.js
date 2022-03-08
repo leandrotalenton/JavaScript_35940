348898a81aa29b054b93d0cd1a6e7cf2ed7a94ad
leandrotalenton <leandrotalenton@hotmail.com>
2022-03-08 18:13:59 -0300

    se optimiza el codigo

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -31,7 +31,7 @@ const allEcoUpgrades = [
     new EcoUpgrade (2,`stone`,`Stone Shaft Mining`,0.48),
 ];
 
-// collection rates, formulas para modificarlas y sus variables necesarias
+// collection rates, niveles de mejora y formulas para modificarlas:
 const collectionRates = {
     food: 0.3383,
     wood: 0.41,
@@ -44,60 +44,30 @@ let woodEcoUpgrade = 1
 let goldEcoUpgrade = 1
 let stoneEcoUpgrade = 1
 
-aumentarFood = function(){
-    collectionRates.food = allEcoUpgrades.filter(I => I.resource === `food`)[foodEcoUpgrade].rate;
-    if(foodEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `food`).length-1){
-        foodEcoUpgrade = 0;
-    } else {
-        foodEcoUpgrade += 1
-    }
+document.querySelector(`.mejorasEconomicas__comida-img`).addEventListener(`click`,() => { // si clickeo la imagen hace las 3 siguientes acciones:
+    document.querySelector(`.mejorasEconomicas__comida-img`).setAttribute(`src`,`./Resources/food${foodEcoUpgrade}.webp`) //cambia el atributo de origen de la imagen
+    collectionRates.food = allEcoUpgrades.filter(I => I.resource === `food`)[foodEcoUpgrade].rate; // reemplaza el valor de recoleccion de comida por el de la enesima mejora de comida del array de mejoras
+    foodEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `food`).length-1 ? foodEcoUpgrade = 0 : foodEcoUpgrade += 1; // aumenta el numero del tracker de mejoras y si estaba en su maximo permitido lo resetea
     newProductionPortfolio()
-}
-aumentarWood = function(){
-    collectionRates.wood = allEcoUpgrades.filter(I => I.resource === `wood`)[woodEcoUpgrade].rate;
-    if(woodEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `wood`).length-1){
-        woodEcoUpgrade = 0;
-    } else {
-        woodEcoUpgrade += 1
-    }
-    newProductionPortfolio()
-}
-aumentarGold = function(){
-    collectionRates.gold = allEcoUpgrades.filter(I => I.resource === `gold`)[goldEcoUpgrade].rate;
-    if(goldEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `gold`).length-1){
-        goldEcoUpgrade = 0;
-    } else {
-        goldEcoUpgrade += 1
-    }
-    newProductionPortfolio()
-}
-aumentarStone = function(){
-    collectionRates.stone = allEcoUpgrades.filter(I => I.resource === `stone`)[stoneEcoUpgrade].rate;
-    if(stoneEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `stone`).length-1){
-        stoneEcoUpgrade = 0;
-    } else {
-        stoneEcoUpgrade += 1
-    }
-    newProductionPortfolio()
-}
-
-document.querySelector(`.mejorasEconomicas__comida-img`).addEventListener(`click`,() => {
-    document.querySelector(`.mejorasEconomicas__comida-img`).setAttribute(`src`,`./Resources/food${foodEcoUpgrade}.webp`)
-    aumentarFood()
-})
+});
 document.querySelector(`.mejorasEconomicas__madera-img`).addEventListener(`click`,() => {
     document.querySelector(`.mejorasEconomicas__madera-img`).setAttribute(`src`,`./Resources/wood${woodEcoUpgrade}.webp`)
-    aumentarWood()
-})
+    collectionRates.wood = allEcoUpgrades.filter(I => I.resource === `wood`)[woodEcoUpgrade].rate;
+    woodEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `wood`).length-1? woodEcoUpgrade = 0 : woodEcoUpgrade += 1;
+    newProductionPortfolio()
+});
 document.querySelector(`.mejorasEconomicas__oro-img`).addEventListener(`click`,() => {
     document.querySelector(`.mejorasEconomicas__oro-img`).setAttribute(`src`,`./Resources/gold${goldEcoUpgrade}.webp`)
-    aumentarGold()
-})
+    collectionRates.gold = allEcoUpgrades.filter(I => I.resource === `gold`)[goldEcoUpgrade].rate;
+    goldEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `gold`).length-1 ? goldEcoUpgrade = 0 : goldEcoUpgrade += 1;
+    newProductionPortfolio()
+});
 document.querySelector(`.mejorasEconomicas__piedra-img`).addEventListener(`click`,() => {
     document.querySelector(`.mejorasEconomicas__piedra-img`).setAttribute(`src`,`./Resources/stone${stoneEcoUpgrade}.webp`)
-    aumentarStone()
-})
-
+    collectionRates.stone = allEcoUpgrades.filter(I => I.resource === `stone`)[stoneEcoUpgrade].rate;
+    stoneEcoUpgrade === allEcoUpgrades.filter(I => I.resource === `stone`).length-1 ? stoneEcoUpgrade = 0 : stoneEcoUpgrade += 1;
+    newProductionPortfolio()
+});
 
 // objeto para almacenar requisitos de recursos por segundo
 let resourcesRquiredPerSecond = {
@@ -105,7 +75,7 @@ let resourcesRquiredPerSecond = {
     wood: 0,
     gold: 0,
     stone: 0,
-    calcularResourcesRquiredPerSecond: function(){
+    calculateResourcesRquiredPerSecond: function(){
         resourcesRquiredPerSecond.food = 0;
         resourcesRquiredPerSecond.wood = 0;
         resourcesRquiredPerSecond.gold = 0;
@@ -136,7 +106,7 @@ let villagerDistribution = {
         woodRequired.innerHTML = (`${villagerDistribution.wood}`);
         goldRequired.innerHTML = (`${villagerDistribution.gold}`);
         stoneRequired.innerHTML = (`${villagerDistribution.stone}`);
-        totalRequired.innerHTML = (`<b>${villagerDistribution.food+villagerDistribution.wood+villagerDistribution.gold+villagerDistribution.stone}</b>`);
+        totalRequired.innerHTML = (`${villagerDistribution.food+villagerDistribution.wood+villagerDistribution.gold+villagerDistribution.stone}`);
     }
 }
 
@@ -145,22 +115,18 @@ let productionPortfolio = [];
 
 //funcion que actualiza el calculo de aldeanos y lo refleja en el HTML
 newProductionPortfolio = function(){
-    productionPortfolio = [];
+    productionPortfolio = []; // vacia el productionPortfolio existente
     for(unit of unitsList){
         let Q = document.querySelector(`.unidades__${unit.name}-q`);
-        if(Q.value === "" || parseInt(Q.value) < 0){ //arregla campos vacios o con Q <0
-            Q.value = 0;
-        }
-        if(parseInt(Q.value) != 0){ //por defecto las imagenes estan oscuras, si Q es >0, se iluminan
-            document.querySelector(`.unidades__${unit.name}-imagen`).classList.add(`activeImg`)
-        } else {
-            document.querySelector(`.unidades__${unit.name}-imagen`).classList.remove(`activeImg`)
-        }
+        (Q.value === "" || parseInt(Q.value) < 0) && (Q.value = 0); //arregla campos vacios o con Q <0
+        parseInt(Q.value) != 0 //por defecto las imagenes estan oscuras, si Q es >0, se iluminan
+            ?document.querySelector(`.unidades__${unit.name}-imagen`).classList.add(`activeImg`)
+            :document.querySelector(`.unidades__${unit.name}-imagen`).classList.remove(`activeImg`)
         for(i = 1; i <= `${parseInt(Q.value)}` ; i++){ //agrega al protfolio de produccion una cantidad de unidades igual a Q
             productionPortfolio.push(unit);
         }
     }
-    resourcesRquiredPerSecond.calcularResourcesRquiredPerSecond();
+    resourcesRquiredPerSecond.calculateResourcesRquiredPerSecond();
     villagerDistribution.calularCantidadDeAldeanos();
     villagerDistribution.mostrarCantidadDeAldeanos();
 }
@@ -201,37 +167,30 @@ let unitsList = [
 
 // creacion del HTML con sus eventos
 for(const unit of unitsList){
-    const unitDiv = document.createElement(`div`);
+    const unitDiv = document.createElement(`div`); // crea un div por unidad
     unitDiv.classList.add(`unidades__${unit.name}`);
     unitDiv.classList.add(`unidades__div`);
 
-    const unitImg = document.createElement(`img`);
+    const unitAmount = document.createElement(`input`); // crea un input por unidad
+    unitAmount.type = `number`
+    unitAmount.placeholder = `Insert amount`
+    unitAmount.classList.add(`unidades__${unit.name}-q`)
+    unitAmount.addEventListener("change", newProductionPortfolio) // si cambio el valor, se ejecuta un newProductionPortfolio
+
+    const unitImg = document.createElement(`img`); // crea un img por unidad
     unitImg.classList.add(`unidades__${unit.name}-imagen`);
     unitImg.classList.add(`unidades__div-imagen`);
-    unitImg.src = unit.image;
+    unitImg.src = unit.image; // le pone el source que figura como propiedad del objeto
     unitImg.setAttribute(`title`,`${unit.name}`)
-    unitImg.onclick = ()=>{
-        if(unitAmount.value === ""){
-            unitAmount.value = 0;
-        }
+    unitImg.onclick = ()=>{ // si clickeo en la imagen se aumenta en 1 al valor del input
         unitAmount.value = parseInt(unitAmount.value) + 1;
         newProductionPortfolio()
     };
 
-    const unitName = document.createElement(`p`);
-
-    const unitAmount = document.createElement(`input`);
-    unitAmount.type = `number`
-    unitAmount.placeholder = `Insert amount`
-    unitAmount.classList.add(`unidades__${unit.name}-q`)
-
-    unitDiv.appendChild(unitImg);
-    unitDiv.appendChild(unitName);
+    unitDiv.appendChild(unitImg); // agrego la image
     unitDiv.appendChild(unitAmount);
 
-    document.querySelector(`.unidades`).appendChild(unitDiv);
-
-    unitAmount.addEventListener("change", newProductionPortfolio)
+    document.querySelector(`.unidades`).appendChild(unitDiv); // agrego las cosas creadas dinamicamente al div existende de mi HTML
 }
 
 document.addEventListener(`DOMContentLoaded` , newProductionPortfolio)
@@ -245,7 +204,7 @@ let guardar = function(){
 document.querySelector(`.botonGuardar`).addEventListener(`click`, guardar);
 
 let cargar = function(){
-    productionPortfolio = JSON.parse(localStorage.getItem(`stringProductionPortfolio`))
+    productionPortfolio = JSON.parse(localStorage.getItem(`stringProductionPortfolio`)) || [];
     for(unit of unitsList){
         document.querySelector(`.unidades__${unit.name}-q`).value = productionPortfolio.filter(propiedadNombre => propiedadNombre.name == unit.name).length;
     }
